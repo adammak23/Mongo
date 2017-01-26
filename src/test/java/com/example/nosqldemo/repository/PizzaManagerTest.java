387b5fff39bf8723f19fdffff45459cdeceb77ce
adammak2342 <adammak2342@o2.pl>
2017-01-26 01:05:32 +0100

    more tests

--- src/test/java/com/example/nosqldemo/repository/PizzaManagerTest.java
+++ src/test/java/com/example/nosqldemo/repository/PizzaManagerTest.java
@@ -45,6 +45,80 @@ public class PizzaManagerTest {
 		pizza=null;
         }
         
-
+	@Test
+	public void checkAdding(){
+                this.clear();
+		Pizza pizza = new Pizza();
+		pizza.setName("Margerita");
+                pizza.setPrice(22);
+                pizza.setDiameter(20);
+                pizza.setSold(false);
+		pizzaManager.persist(pizza);
+		pizza=null;
+		pizza = new Pizza();
+		pizza.setName("Carbonara");
+                pizza.setPrice(222);
+                pizza.setDiameter(20);
+                pizza.setSold(false);
+		pizzaManager.persist(pizza);
+		pizza=null;
+		pizza = new Pizza();
+		pizza.setName("Hawajska");
+                pizza.setPrice(2222);
+                pizza.setDiameter(15);
+                pizza.setSold(false);
+		pizzaManager.persist(pizza);
+		List<Pizza> cars = pizzaManager.getpizzas("Margerita");
+		
+		assertTrue(cars.size() >= 1);
+	}
+        
+        @Test 
+        public void checkingEdit() {
+            Pizza pizza = pizzaManager.getpizzas("Carbonara").get(0);
+            pizza.setName("Hawajska");
+            pizza.setSold(true);
+            pizzaManager.persist(pizza);
+            pizza = null;
+            
+            pizza = pizzaManager.getpizzas("Hawajska").get(0);
+            assertEquals(pizza.getName(), "Hawajska");
+            assertEquals(pizza.getPrice(), 22.0, 0.01);
+            assertEquals(pizza.getDiameter(), 20);
+            assertEquals(pizza.getSold(), true);
+        }
+        
+        @Test
+        public void checkingRemove() {
+                this.clear();
+		Pizza pizza = new Pizza();
+		pizza.setName("Margerita");
+                pizza.setPrice(22);
+                pizza.setDiameter(20);
+                pizza.setSold(false);
+		pizzaManager.persist(pizza);
+		pizza=null;
+		pizza = new Pizza();
+		pizza.setName("Carbonara");
+                pizza.setPrice(222);
+                pizza.setDiameter(20);
+                pizza.setSold(false);
+		pizzaManager.persist(pizza);
+                
+                pizza=null;
+                pizza = pizzaManager.getpizzas("Margerita").get(0);
+                pizzaManager.remove(pizza);
+                
+                List<Pizza> list = (List<Pizza>) pizzaManager.getAll();
+                pizza = list.get(0);
+                
+                assertEquals(pizza.getName(), "Carbonara");
+                assertEquals(pizza.getPrice(), 222.0, 0.01);
+                assertEquals(pizza.getDiameter(), 20);
+                assertEquals(pizza.getSold(), false);
+        }
+        
+        
+       
 
 }
